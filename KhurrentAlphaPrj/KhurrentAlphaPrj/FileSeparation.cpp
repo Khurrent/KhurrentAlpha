ff59403579448344c3c97154795b476eb5a38f82
Byunghoon Lim <seian@me.com>
2013-08-24 01:47:40 +0900

    Get File Size & split preparation

--- KhurrentAlphaPrj/KhurrentAlphaPrj/FileSeparation.cpp
+++ KhurrentAlphaPrj/KhurrentAlphaPrj/FileSeparation.cpp
@@ -1,29 +1,61 @@
 #include "FileSeparation.h"
+
 #include <iostream>
-#include <fstream>
+#include <stdlib.h>
 #include <string>
 using namespace std;
 
 FileSeparation::FileSeparation()
 {
+	/* initialize file split size */
+	this->_splitSize = 0;
 }
 
 FileSeparation::~FileSeparation()
 {
-	cout << _address;
+	cout << "file Size : " << this->_orgFileSize << endl;
+	cout << "file Name : " << this->_address << endl;
 }
 
 void FileSeparation::setFileAddress(char *addr)
 {
 	this->_address = addr;
+	this->_splitSize = 0;
 }
 
-void FileSeparation::setSplitSize(int size)
+void FileSeparation::setSplitSize(size_t size)
 {
-	this->_pieceSize = size;
+	this->_splitSize = size;
 }
 
 bool FileSeparation::fileSplit()
 {
+	FILE *fp;
+
+	readFile(fp);
+	fclose(fp);
 	return false;
+}
+
+void FileSeparation::readFile(FILE *&fp)
+{
+	/* open File */
+	fp = fopen(this->_address, "rb");
+	/* get Position to calculate File Size */
+	this->_orgFileSize = getFileSize(fp);
+}
+
+
+unsigned long long int FileSeparation::getFileSize(FILE *&fp)
+{
+	unsigned long long int fSize;
+	/* get File size. Move to EOF */
+	fseek(fp, 0, SEEK_END);
+	/* get File Size */
+	fSize = ftell(fp);
+	/* rewind current position to the front */
+	rewind(fp);
+
+	/* return file size */
+	return fSize;
 }
--- KhurrentAlphaPrj/KhurrentAlphaPrj/FileSeparation.h
+++ KhurrentAlphaPrj/KhurrentAlphaPrj/FileSeparation.h
@@ -1,34 +1,47 @@
 #ifndef _FILE_SEPARATION_H
 #define _FILE_SEPARATION_H
 
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
 class FileSeparation
 {
 public:
-	//split units(16KB ~ 16MB to be BYTES) 
-	static const int KBYTE_16		=	16 * 1024;
-	static const int KBYTE_32		=	32 * 1024;
-	static const int KBYTE_64		=	64 * 1024;
-	static const int KBYTE_128		=	128 * 1024;
-	static const int KBYTE_256		=	256 * 1024;
-	static const int KBYTE_512		=	512 * 1024;
-	static const int KBYTE_1024		=	1024 * 1024;
-	static const int KBYTE_2048		=	2048 * 1024;
-	static const int KBYTE_4096		=	4096 * 1024;
-	static const int KBYTE_8192		=	8192 * 1024;
-	static const int KBYTE_16384		=	16384 * 1024;
+	/* split units(16KB ~ 16MB to be BYTES) */
+	static const size_t KBYTE_16		=	16 * 1024;
+	static const size_t KBYTE_32		=	32 * 1024;
+	static const size_t KBYTE_64		=	64 * 1024;
+	static const size_t KBYTE_128		=	128 * 1024;
+	static const size_t KBYTE_256		=	256 * 1024;
+	static const size_t KBYTE_512		=	512 * 1024;
+	static const size_t KBYTE_1024		=	1024 * 1024;
+	static const size_t KBYTE_2048		=	2048 * 1024;
+	static const size_t KBYTE_4096		=	4096 * 1024;
+	static const size_t KBYTE_8192		=	8192 * 1024;
+	static const size_t KBYTE_16384	=	16384 * 1024;
 
-	//constructors & destructor
+	/* constructors & destructor */
 	FileSeparation();
 	FileSeparation(char *addr) : _address(addr) {}
-	FileSeparation(char *addr, int pieceSize) : _address(addr), _pieceSize(pieceSize) {}
+	FileSeparation(char *addr, int splitSize) : _address(addr), _splitSize(splitSize) {}
 	~FileSeparation();
 
 	void setFileAddress(char *addr);
-	void setSplitSize(int size);
+	void setSplitSize(size_t size);
 	bool fileSplit();
 private:
+	char *_fileData;
 	char *_address;
-	int _pieceSize;
+	size_t _splitSize;
+	unsigned long long int _orgFileSize;
+
+	/* readFile (open) */
+	void readFile(FILE *&fp);
+	/* get file size using fseek() */
+	unsigned long long int getFileSize(FILE *&fp);
 };
 
 #endif
